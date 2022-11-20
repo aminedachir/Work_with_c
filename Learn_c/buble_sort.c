435eff7991a938954914da3205d8276c6b44350c
aminedachir <dachiramine110>
2022-11-20 11:06:55 +0100

    buble sort

--- Learn_c/buble_sort.c
+++ Learn_c/buble_sort.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-int arr[4] = {6,82,300,7};
+int arr[6] = {5,2,1,3,6,4};
 int z;
 int main ()
 {
-    for (int i=0;i<4;i++){
-        for (int j=i+1;j<4;j++){
-            if(arr[j]<arr[i]){
-                z = arr[i];
-                arr[i] = arr[j];
+    for (int i=0;i<6;i++){
+        for (int j=0;j<5;j++){
+            if(arr[j+1]<arr[j]){
+                z = arr[j+1];
+                arr[j+1] = arr[j];
                 arr[j] = z;
             }
         }
     }
-    for(int h=0;h<4;h++){
+    for(int h=0;h<6;h++){
         printf("%d\n",arr[h]);
     }
 }
