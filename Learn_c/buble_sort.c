1ac8a1e5a86ee222a40a11bee8bcc65ad794f48d
aminedachir <dachiramine110>
2022-11-20 10:49:55 +0100

    buble sort

---
+++ Learn_c/buble_sort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int arr[4] = {6,82,300,7};
+int z;
+int main ()
+{
+    for (int i=0;i<4;i++){
+        for (int j=i+1;j<4;j++){
+            if(arr[j]<arr[i]){
+                z = arr[i];
+                arr[i] = arr[j];
+                arr[j] = z;
+            }
+        }
+    }
+    for(int h=0;h<4;h++){
+        printf("%d\n",arr[h]);
+    }
+}
