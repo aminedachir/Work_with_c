cb0be2c128c4a538132516c3d9fb76e399f43291
aminedachir <dachiramine110>
2022-12-13 20:51:52 +0100

    problem

---
+++ Learn_c/Strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+char mot[50];
+int s=0;
+int main()
+{
+    scanf("%c",&mot);
+    for(int i=0;i<strlen(mot);i++){
+        char h[strlen(mot)] = ;
+    }
+    printf("%d",s);
+    if (s == strlen(mot)){
+        printf("match");
+    }
+}
