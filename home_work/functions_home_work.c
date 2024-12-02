bb68642f5918f3528a1baf93d1d76644a6511ac1
aminedachir <dachiramine110@gmail.com>
2024-12-02 03:03:15 +0100

    add the merge sort

--- home_work/functions_home_work.c
+++ home_work/functions_home_work.c
@@ -25,10 +25,6 @@ int Put_Matrix_in_array(int n,int m,int Matrix[n][m],int arr[n*m]){
             k++;
         }
     }
-    for (int i = 0; i < n*m; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
 }
 void insertionSort(int arr[],int n) {
     for (int i = 1; i < n; i++) {
@@ -39,6 +35,39 @@ void insertionSort(int arr[],int n) {
             j = j - 1;
         }
         arr[j + 1] = z;
+    }
+}
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int leftArr[n1], rightArr[n2];
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        rightArr[j] = arr[mid + 1 + j];
+    }
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k++] = leftArr[i++];
+        } else {
+            arr[k++] = rightArr[j++];
+        }
+    }
+    while (i < n1) {
+        arr[k++] = leftArr[i++];
+    }
+    while (j < n2) {
+        arr[k++] = rightArr[j++];
+    }
+}
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 int Put_array_in_Matrix(int n,int m,int Matrix[n][m],int arr[n*m]){
--- home_work/home_work.c
+++ home_work/home_work.c
@@ -7,14 +7,21 @@ int main(){
     int n,m;
     int arr[n*m];
     int Matrix[n][m];
-    printf("Enter the number of rows in matrix");
+    printf("Enter the number of rows in matrix : ");
     scanf("%d",&n);
-    printf("Enter the number of Cols in matrix");
+    printf("Enter the number of Cols in matrix : ");
     scanf("%d",&m);
     Register_Matrix(n,m,Matrix);
     Print_Matrix_V0(n,m,Matrix);
     Put_Matrix_in_array(n,m,Matrix,arr);
+    mergeSort(arr, 0, n * m - 1);
+    Put_array_in_Matrix(n,m,Matrix,arr);
+    Print_Matrix(n,m,Matrix);
+    printf("<-------- done it with mergeSort -------->\n");
     insertionSort(arr,n*m);
     Put_array_in_Matrix(n,m,Matrix,arr);
     Print_Matrix(n,m,Matrix);
+     printf("<-------- done it with insertionSort -------->\n");
+
+    
 }
